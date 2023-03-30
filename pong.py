cfc3709927d6e36b194083af5c8cbd5cc90c2c2d
Mohanavarsha <77161313+Mohanavarsha@users.noreply.github.com>
2023-03-30 21:28:35 +0530

    Update pong.py

--- pong.py
+++ pong.py
@@ -90,12 +90,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1 
-        winsound.PlaySound("sound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
     
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1 
-        winsound.PlaySound("sound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
 
     
     if ball.xcor() > 390:
@@ -117,13 +117,13 @@ while True:
     #Paddle and Ball
     if (ball.xcor() > 340 and ball.xcor() < 350)and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
-        winsound.PlaySound("sound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
         ball.dx *= -1
         
     
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
-        winsound.PlaySound("sound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
         ball.dx *= -1
 
     if scoreA >= 3:
